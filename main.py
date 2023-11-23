3b28462175fd36b977d00aaf85792e679584a192
Daalen <tim.vandaalen@wur.nl>
2023-11-23 19:48:53 +0100

    add debug code

--- main.py
+++ main.py
@@ -5,13 +5,25 @@ from time import sleep
 from copy import deepcopy
 from scipy.spatial.transform import Rotation as R
 
+# -------------------------------
+# Simple program intended to correct to photoneo pointclouds
+# It start by opening a 3D viewer which allows users to pick 3 points.
+# The first 3 should be on the rockwool and are used for the rotational correction.
+# This is done with the align_vectors function of scipi, alternatively the normals of the rockwool block can be used.
+# The last one is used as new origin for the pointcloud
+# Based on: http://www.open3d.org/docs/latest/tutorial/Basic/transformation.html
+# --------------------------------
+
+
 def pick_points(pcd):
+    """Üse the interactive vizualisation of open3d to enable users to select points """
     print("")
     print("1) Please pick at least three points on the cube and than you origin point using [shift + left click]")
     print("   Press [shift + right click] to undo point picking")
     print("2) Afther picking points, press q for close the window")
     print("3) Use shift + '+' or '-' to change the marker size")
-    # See here for the other controls: https://github.com/isl-org/Open3D/blob/d7341c4373e50054d9dbe28ed84c09bb153de2f8/src/Visualization/Visualizer/VisualizerWithEditing.cpp#L124
+    # For the other controls see: 
+    # https://github.com/isl-org/Open3D/blob/d7341c4373e50054d9dbe28ed84c09bb153de2f8/src/Visualization/Visualizer/VisualizerWithEditing.cpp#L124
 
     vis = o3d.visualization.VisualizerWithEditing()
     vis.create_window()
@@ -20,6 +32,15 @@ def pick_points(pcd):
     vis.destroy_window()
     print("")
     return vis.get_picked_points()
+
+def vizualize_rotation():
+    """"Create a reference frame and rotate/move it like the pointcloud will rotate, for debugging purposes only"""
+    inv_rotation, inv_rssd = R.align_vectors(b=target_vectors, a=source_vectors, weights=weights)
+    #rot_vec = rotation.as_rotvec()
+    ref_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=25, origin=[0, 0, 0])
+    ref_frame_rot = deepcopy(ref_frame)
+    ref_frame_rot.rotate(inv_rotation.as_matrix(), center=(0, 0, 0))
+    o3d.visualization.draw_geometries([ref_frame, ref_frame_rot])
 
 
 if __name__ == '__main__':
@@ -48,6 +69,7 @@ if __name__ == '__main__':
         weights = np.array([0.7, 0.3])
         # Rotation vector b to vector a
         # https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.transform.Rotation.align_vectors.html#scipy.spatial.transform.Rotation.align_vectors
+        #vizualize_rotation()
         rotation, rssd = R.align_vectors(a=target_vectors, b=source_vectors, weights=weights)
         rot_vec = rotation.as_rotvec()
         pcd_r = deepcopy(pcd)
